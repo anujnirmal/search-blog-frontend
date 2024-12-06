f1472a031c37c3eb055587963e26a91b7a446fe6
Anuj Nirmal <anujrajeshnirmal@gmail.com>
2024-12-06 16:46:09 +0530

    bug fix

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import BlogCard from "./BlogCard";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
